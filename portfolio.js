59d560dec7763efbff612a873d0e7bc46a50a6bc
antoniobusrod <antoniobusrod@gmail.com>
2014-10-05 16:29:27 +0200

    minor changes, refactor code

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,16 @@
-var sections = [
-  'personal-data',
-  'experience',
-  'education',
-  'computing-skills',
-  'research',
-  'misc'
-];
+var sections = {
+	'personal-data'    : true,
+	'experience'       : true,
+	'education'        : true,
+	'computing-skills' : true,
+	'research'         : true,
+	'misc'             : true,
+};
+var queryParamId = 'q';
 
-var sectionSelected = getUrlParams()['q'];
-if (sectionSelected) {
-  if (sections.indexOf(sectionSelected) >= 0) {
-    toggle(sectionSelected.toString());
-  }
+var sectionSelected = getUrlParams()[queryParamId];
+if (!!sectionSelected && !!sections[sectionSelected]) {
+	toggle(sectionSelected.toString());
 }
 
 // Local functions
@@ -20,16 +19,16 @@ function getUrlParams() {
   var regexp = /[?&]([^=#]+)=([^&#]*)/g;
   var params = {};
   var matches;
-  while (matches = regexp.exec(window.location.href)) {
+  while (!!(matches = regexp.exec(window.location.href))) {
     params[matches[1]] = matches[2];
   }
   return params;
 }
 
 function toggle(id) {
-  sections.forEach(function(section) {
+	for (var section in sections) {
     document.getElementById(section).style.display = "none";
-  });
+	}
   document.getElementById(id).style.display = "block";
 }
 
